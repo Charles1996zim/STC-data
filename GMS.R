4cb254410489283967594c8854a6690bed26d401
Charles1996zim <cm.randell@yahoo.com>
2022-12-02 07:51:09 +0200

    Overhaul - tickets to blending

--- GMS.R
+++ GMS.R
@@ -9,4 +9,8 @@ con <- DBI::dbConnect(odbc::odbc(),
                       server = "192.168.8.114", # 127.0.0.1
                       database = "NT GMS.fmp12",
                       uid = "CHARLESR",
-                      pwd = "Temp837%")
+                      pwd = "Temp123!")
+
+
+
+DBI::dbGetQuery(con, 'SELECT COUNT(*) FROM Accounts Transaction Items')
--- STC-data.R
+++ STC-data.R
@@ -3,44 +3,191 @@ library(odbc)
 library(dbplyr)
 library(DBI)
 
+options("scipen"=10) 
+
+
 
 con <- DBI::dbConnect(odbc::odbc(),
                       driver = "FileMaker ODBC",
-                      server = "192.168.8.114", # 127.0.0.1
+                      server = "db02.nt.co.zw", # 127.0.0.1
                       database = "STC.fmp12",
                       uid = "CRandell",
                       pwd = "Ngenile!99")
 
-DBI::dbGetQuery(con, 'SELECT grade_internal FROM TICKETS FETCH FIRST 100 ROWS ONLY')
-
-DBI::dbGetQuery(con, 'SELECT COUNT(*) FROM TICKETS')
 
 
+# CREATE FILE WITH ALL TICKETS
 
-grades_reclass <-
-DBI::dbGetQuery(con, 
-                'SELECT grade_internal, grade_internal_reclass 
-                FROM TICKETS') %>% as_tibble()
+tickets <-
+DBI::dbGetQuery(con, paste0("SELECT * FROM TICKETS"))
 
+# TURN TABLE INTO TIBBLE
 
-grades_reclass %>% filter(grade_internal != grade_internal_reclass) %>% group_by(grade_internal, grade_internal_reclass) %>%
-  summarise(n = n()) %>% arrange(desc(n))
+tickets1 <- tickets %>% as_tibble()
 
+# FILTERS TO OBTAIN ALL COMMERICAL AND SS SALES
 
-grades_purch <-
-  DBI::dbGetQuery(con, 
-                  'SELECT grade_internal, grade_internal_reclass, number_price
-                FROM TICKETS') %>% as_tibble()
-
-grades_purch %>% filter(grade_internal != grade_internal_reclass) %>% group_by(grade_internal, grade_internal_reclass) %>%
-  summarise(n = n()) %>% arrange(desc(n)) %>% 
-  left_join(grades_purch %>% group_by(grade_internal) %>% summarise(internal = mean(number_price)), by = "grade_internal") %>%
-  left_join(grades_purch %>% group_by(grade_internal) %>% summarise(reclass = mean(number_price)), by = c("grade_internal_reclass" = "grade_internal")) %>%
-  mutate(price_diff = reclass - internal) %>% mutate(value_change = n*price_diff) %>% rmsfuns::ViewXL()
+tickets1 %>%
+  group_by(grade_internal, grade_internal_reclass, location_current) %>% 
+  summarise(total = sum(mass), value = sum(calc_total_value_dollars)) %>% 
+  filter(grade_internal != "TBDAU") %>%
+  filter(location_current %in% c("Bay 8-9", "TPZ", "Transit - Bay 8-9", "Receiving C", "REC C (TPZ)", " TPZ", "TPZ ")) %>%
+  rmsfuns::ViewXL()
 
 
+tickets1 %>%
+  filter(grade_internal != "TBDAU") %>% 
+  filter(location_current %in% c("Bay 8-9", "TPZ", "Transit - Bay 8-9", "Receiving C", "REC C (TPZ)", " TPZ", "TPZ ")) %>%
+  rmsfuns::ViewXL()
 
-grades_purch %>% group_by(grade_internal_reclass) %>% summarise(m = mean(number_price, na.rm = TRUE)) %>% rmsfuns::ViewXL()
+
+# AUCTION & HANDSTRIP
+
+tickets1 %>% group_by(grade_internal, grade_internal_reclass, location_current, status_processed) %>% 
+  summarise(total = sum(mass), value = sum(calc_total_value_dollars)) %>% 
+  filter(grade_internal == "TBDAU" | grade_internal_reclass == "TBDAU") %>% 
+  filter(!grepl("/", grade_internal_reclass)) %>%
+  filter(!grepl("S", grade_internal_reclass)) %>%
+  filter(!grepl("H", grade_internal_reclass)) %>%
+  filter(!grepl("MTC", location_current)) %>%
+  rmsfuns::ViewXL()
+
+tickets1 %>% group_by(grade_internal, grade_internal_reclass, location_current, status_processed) %>% 
+  summarise(total = sum(mass), value = sum(calc_total_value_dollars)) %>% 
+  filter(grade_internal == "TBDAU" | grade_internal_reclass == "TBDAU") %>% 
+  filter(!grepl("/", grade_internal_reclass)) %>%
+  filter(!grepl("MTC", location_current)) %>% ungroup() %>%
+  mutate(location_current = trimws(location_current)) %>%
+  group_by(location_current) %>% summarize(total = sum(total), value = sum(value)) %>%
+  rmsfuns::ViewXL()
 
 
+auction_man <- readxl::read_excel("C:/Users/charlesr/Documents/Green Reconciliation/Auction data final.xlsx") %>% janitor::clean_names()
+
+
+auction_man %>% left_join(tickets1 %>% select("_ext_number_ticket", 
+                                              grade_internal, grade_internal_reclass, location_current, status_processed, mass, calc_total_value_dollars), 
+                          by = c("nt_barcode" = "_ext_number_ticket")) %>% rmsfuns::ViewXL()
+
+
+tickets1 %>%
+  filter(grade_internal == "TBDAU" | grade_internal_reclass == "TBDAU") %>% 
+  filter(!grepl("/", grade_internal_reclass)) %>%
+  filter(!grepl("MTC", location_current)) %>% select("_ext_number_ticket", 
+                                                      grade_internal, grade_internal_reclass, location_current, status_processed, mass, calc_total_value_dollars) %>%
+  rmsfuns::ViewXL()
+
+
+tickets1
+
+tickets1 %>% group_by(grade_internal, grade_internal_reclass, location_current) %>% 
+  summarise(total = sum(mass), value = sum(calc_total_value_dollars)) %>% 
+  filter(grade_internal != "TBDAU") %>% 
+  filter(location_current %in% c("Bay 8-9", "TPZ", "Transit - Bay 8-9", "Receiving C", "REC C (TPZ)", " TPZ", "TPZ ")) %>% 
+  ungroup() %>%
+  mutate(location_current = trimws(location_current)) %>%
+  group_by(location_current) %>% summarize(total = sum(total), value = sum(value)) %>%
+  rmsfuns::ViewXL()
+
+
+
+
+
+
+# PROCESSING RUNS
+
+
+processing <-
+  DBI::dbGetQuery(con, paste0("SELECT * FROM processing_blends")) %>% as_tibble()
+
+
+processing_runs <-
+  DBI::dbGetQuery(con, paste0("SELECT * FROM processing_runs")) %>% as_tibble()
+
+
+
+
+
+# LINKING TICKET NUMBER TO BLEND PROCESSSING
+
+# ONLY NON AUCTION
+
+tickets1 %>% left_join(processing %>% select("__kp_processing_blend_id", grade_processed), by = c("_ext_processing_blend_id"="__kp_processing_blend_id")) %>%
+  group_by(grade_internal, grade_internal_reclass, location_current, grade_processed) %>% 
+  filter(grade_internal != "TBDAU") %>% 
+  filter(location_current %in% c("Bay 8-9", "TPZ", "Transit - Bay 8-9", "Receiving C", "REC C (TPZ)", " TPZ", "TPZ ")) %>%
+  group_by(grade_internal, grade_internal_reclass, grade_processed) %>% 
+  summarise(total = sum(mass), value = sum(calc_total_value_dollars)) %>% 
+  rmsfuns::ViewXL()
+
+
+# ADJUSTS FOR MTC SWAP 
+
+# ADDS AUCTION DATA BY ELIMINATING TICKETS WITH NO BLEND ID IN AUCITON VOLUME
+
+
+  tickets1 %>% left_join(processing %>% select("__kp_processing_blend_id", grade_processed), by = c("_ext_processing_blend_id"="__kp_processing_blend_id")) %>%
+    filter(!grepl("MTC", grade_processed)) %>%
+    group_by(grade_internal, grade_internal_reclass, location_current, grade_processed) %>% 
+    filter(grade_internal != "TBDAU") %>% 
+    filter(location_current %in% c("Bay 8-9", "TPZ", "Transit - Bay 8-9", "Receiving C", "REC C (TPZ)", " TPZ", "TPZ ")) %>%
+    group_by(grade_internal, grade_internal_reclass, grade_processed) %>% 
+    summarise(total = sum(mass), value = sum(calc_total_value_dollars)) %>%
+    bind_rows(tickets1 %>% left_join(processing %>% select("__kp_processing_blend_id", grade_processed), by = c("_ext_processing_blend_id"="__kp_processing_blend_id")) %>%
+                filter(grade_internal == "TBDAU" | grade_internal_reclass == "TBDAU") %>% 
+                filter(!grepl("/", grade_internal_reclass)) %>%
+                filter(!grepl("MTC", grade_processed)) %>%
+                filter(!grepl("HANDSTRIP", grade_processed)) %>%
+                filter(!grepl("SCRAP", grade_processed)) %>%
+                filter(!grepl("HSPICK", grade_processed)) %>%
+                filter(grade_processed != "NA") %>%
+                group_by(grade_internal, grade_internal_reclass, location_current, grade_processed) %>%
+                summarise(total = sum(mass), value = sum(calc_total_value_dollars)) %>%
+                ungroup() %>% select(!location_current)) %>% 
+    rmsfuns::ViewXL()
+
+  
+  
+  # THIS SHOWS WHERE ALL CONTRACT FLOOR TICKETS WERE ASSIGNED DURING PROCESSING 
+  
+  tickets1 %>%
+    left_join(processing %>% select("__kp_processing_blend_id", grade_processed), by = c("_ext_processing_blend_id"="__kp_processing_blend_id")) %>%
+    filter(grade_internal != "TBDAU") %>%
+    filter(location_current %in% c("Bay 8-9", "TPZ", "Transit - Bay 8-9", "Receiving C", "REC C (TPZ)", " TPZ", "TPZ ")) %>%
+    group_by(grade_processed) %>% summarise(total = sum(mass), value = sum(calc_total_value_dollars)) %>% rmsfuns::ViewXL()
+
+
+  
+# FINAL PRICE COMPARISON - WITH COMMERCIAL AND AUCTION DATA 
+  
+  
+  
+  
+  tickets %>% rename(ext_processing_run_id = "_ext_processing_run_id") %>%
+    filter(grade_internal != "TBDAU") %>% rename(ext_processing_blend_id = "_ext_processing_blend_id") %>%
+    filter(location_current %in% c("Bay 8-9", "TPZ", "Transit - Bay 8-9", "Receiving C", "REC C (TPZ)", " TPZ", "TPZ ")) %>%
+    group_by(ext_processing_run_id, ext_processing_blend_id) %>% summarise(mass = sum(mass, na.rm = TRUE), value = sum(calc_total_value_dollars)) %>%
+    left_join(processing %>% select("__kp_processing_blend_id", grade_processed), by = c("ext_processing_blend_id"="__kp_processing_blend_id")) %>%
+    filter(!grepl("MTC", grade_processed)) %>%
+    select(mass, value, "ext_processing_run_id", grade_processed) %>%
+    left_join(tickets %>% rename(ext_processing_run_id = "_ext_processing_run_id") %>%
+                filter(grade_internal == "TBDAU" | grade_internal_reclass == "TBDAU") %>% 
+                left_join(processing %>% select("__kp_processing_blend_id", grade_processed), by = c("_ext_processing_blend_id"="__kp_processing_blend_id")) %>%
+                filter(!grepl("/", grade_internal_reclass)) %>%
+                filter(!grepl("MTC", grade_processed)) %>%
+                filter(!grepl("HANDSTRIP", grade_processed)) %>%
+                filter(!grepl("SCRAP", grade_processed)) %>%
+                filter(!grepl("HSPICK", grade_processed)) %>%
+                filter(grade_processed != "NA") %>%
+                group_by(ext_processing_run_id) %>% summarise(mass2 = sum(mass, na.rm = TRUE), value2 = sum(calc_total_value_dollars)) %>%
+                select(mass2, value2, "ext_processing_run_id"), by = c("ext_processing_run_id")) %>%
+    mutate(mass2 = ifelse(is.na(mass2), 0, mass2)) %>%
+    mutate(value2 = ifelse(is.na(value2), 0, value2)) %>%
+    mutate(mass = mass+mass2, value = value+value2) %>% select(-mass2, -value2) %>%
+    left_join(processing_runs %>% select("__kp_processing_run_id", calc_mass, calc_total_value_dollars, calc_average_price) %>%
+              rename("kp_processing_run_id" = "__kp_processing_run_id"), 
+              by = c("ext_processing_run_id" = "kp_processing_run_id")) %>%
+    select(3, 4, everything()) %>% rmsfuns::ViewXL()
+    
+    
 
